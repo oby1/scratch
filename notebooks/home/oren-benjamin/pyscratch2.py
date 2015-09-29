ff1051c68030a1091fe598f22b16c0167a6f75a3
Oren Benjamin <halcyon@gmail.com>
2015-09-29 11:42:29 -0400

    Initial commit

---
+++ notebooks/home/oren-benjamin/pyscratch2.py
@@ -0,0 +1,21 @@
+# Databricks notebook source exported at Tue, 29 Sep 2015 15:42:28 UTC
+# MAGIC %md Github integration test
+
+# COMMAND ----------
+
+df = sqlContext.createDataFrame([(1, 4), (2, 5), (3, 6)], ['A', 'B'])
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
